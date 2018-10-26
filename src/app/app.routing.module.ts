c5a4b727a4fe86ec5bef38fc26e1b02c5494f540
Naresh Rawat <nareshrawat33@gmail.com>
2018-10-26 12:43:33 +0530

    routing module created

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from 'src/app/search/search.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from 'src/app/home/home.component';
+
+const appRoutes:Routes = [
+    {path:'', component:HomeComponent, pathMatch:'full'},
+    {path:'search', component:SearchComponent},  
+    {path:'**', redirectTo:''}
+];
+@NgModule({
+    imports:[RouterModule.forRoot(appRoutes)],
+    exports:[RouterModule]
+})
+export class AppRoutingModule {
+    
+}
