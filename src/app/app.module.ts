c7da4867bacdc3adf782ed982f5d43211c8e8d2d
Naresh Rawat <nareshrawat33@gmail.com>
2018-10-26 12:43:20 +0530

    required modules were added and components were imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {PaginatorModule} from 'primeng/paginator';
 
+import { CoreModule } from './core/core.module';
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from 'src/app/search/search.component';
+import { HomeComponent } from 'src/app/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    PaginatorModule,
+    CoreModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
