f332a837319755c067f5f634f9e2bd527164c930
Naresh Rawat <nareshrawat33@gmail.com>
2018-10-26 12:43:48 +0530

    core module created

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { ApiService } from './services/api.service';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+
+@NgModule({
+  imports: [
+      HttpClientModule
+  ],
+  declarations: [],
+  providers:[
+    ApiService,
+  ]
+})
+export class CoreModule { }
